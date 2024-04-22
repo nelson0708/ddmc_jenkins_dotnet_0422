00859fe3e0fc03f77b0a1a0494a74b8a59641313
Nelson <chuellee@gmail.colm>
2024-04-22 13:53:28 +0800

    加增新功能

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to C# Programming!");
+Console.WriteLine("如果新功能有了Unicode, 你可以直接使用中文了!");
+Console.WriteLine("你好，世界！");
+Console.WriteLine("欢迎来到C#编程！");
+Console.WriteLine("如果新功能有了Unicode, 你可以直接使用中文了!");
